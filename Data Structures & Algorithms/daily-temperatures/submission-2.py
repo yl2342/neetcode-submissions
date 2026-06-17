3b448a74402fb62e8b477ff8a7bac49c893719d8
YUNTIAN LIU <107713898+yl2342@users.noreply.github.com>
2026-06-17 00:26:01 -0400

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # monotonious decreasing stack
+        # set result to 0 for default
+        res = [0] * len(temperatures)
+        stack = [] # store a tuple for both index and temperature (index, temp)
+        for i, t in enumerate(temperatures):
+            while stack and stack[-1][1]< t:
+                (index, temp) = stack.pop() # each pop indicate we find an increase since last peak: t > last
+                res[index] = i - index # ! beauty of it
+            stack.append((i,t))
+        return res 
+        
+        
